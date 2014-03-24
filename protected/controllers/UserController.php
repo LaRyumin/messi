724d4b81714499560630ed8d11d5e5d5f2d40036
alex <alex@alex>
2014-03-24 11:31:20 +0200

    Transfer userlogin from SiteController to UserController

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -1,6 +1,7 @@
 <?php
 
 class UserController extends GxController {
+
   public function actionView($id) {
     $this->render('view', array(
       'model' => $this->loadModel($id, 'User'),
@@ -72,6 +73,37 @@ class UserController extends GxController {
     $this->render('admin', array(
       'model' => $model,
     ));
+  }
+
+  /**
+   * Displays the login page
+   */
+  public function actionLogin() {
+    $model = new LoginForm;
+
+    // if it is ajax validation request
+    if (isset($_POST['ajax']) && $_POST['ajax'] === 'login-form') {
+      echo CActiveForm::validate($model);
+      Yii::app()->end();
+    }
+
+    // collect user input data
+    if (isset($_POST['LoginForm'])) {
+      $model->attributes = $_POST['LoginForm'];
+      // validate user input and redirect to the previous page if valid
+      if ($model->validate() && $model->login())
+        $this->redirect(Yii::app()->user->returnUrl);
+    }
+    // display the login form
+    $this->render('login', array('model' => $model));
+  }
+
+  /**
+   * Logs out the current user and redirect to homepage.
+   */
+  public function actionLogout() {
+    Yii::app()->user->logout();
+    $this->redirect(Yii::app()->homeUrl);
   }
 
 }
