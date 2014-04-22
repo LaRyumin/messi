9ea81f7fb024a5d884e88313bb015e783bd54000
alex <alex@alex>
2014-04-22 12:21:50 +0300

    filters

--- protected/controllers/FilterController.php
+++ protected/controllers/FilterController.php
@@ -5,7 +5,52 @@ class FilterController extends Controller {
   public function actionIndex() {
     $min_price = 150;
     $max_price = 600;
-    $this->render('index', array('min_price' => $min_price, 'max_price' => $max_price));
+    $rating = array(5, 4, 3, 2, 1);
+    $cuisine = array(
+      'Европейская',
+      'Украинская',
+      'Русская',
+      'Суши-бар',
+      'Японская',
+      'Итальянская',
+      'Пивной ресторан',
+      'Восточная',
+      'Сэндвич бар',
+    );
+    $cuisine_more = array(
+      'Авторская',
+      'Китайская',
+      'Грузинская',
+      'Средиземноморская',
+      'Узбекская',
+      'Французская',
+      'Армянская',
+      'Латиноамериканская',
+      'Мексиканская',
+      'Американская',
+      'Тайская',
+      'Рыбная',
+      'Скандинавская',
+    );
+    $services = array(
+      'Wi-Fi',
+      'Завтрак',
+      'Парковка',
+      'Оплата (картами, наличными, мыть посуду)',
+      'Спортивные трансляции',
+      'Детское меню',
+      'Детская комната',
+      'Живая музыка',
+      'Проведение банкетов',
+    );
+    $this->render('index', array(
+      'min_price' => $min_price,
+      'max_price' => $max_price,
+      'rating' => $rating,
+      'cuisine' => $cuisine,
+      'cuisine_more' => $cuisine_more,
+      'services' => $services,
+    ));
   }
 
 }
--- protected/views/filter/index.php
+++ protected/views/filter/index.php
@@ -1,8 +1,8 @@
-<div class="filter-aside">
-  <div class="filter-aside-content">
+<form action="POST">
+  <div class="filter-aside">
+    <div class="filter-aside-content">
 
-    <div class="filter-aside-price">
-      <form class="formula" action="#" method="post">
+      <div class="filter-aside-price">
         <div class="form-cost">
           <h4>Цена среднего чека</h4>
           <input type="text" id="min-cost" value="<?= $min_price ?>"/> -
@@ -13,30 +13,91 @@
           <div id="slider"></div>
         </div>
         <div class="clear"></div>
-      </form>
-    </div>
+      </div>
 
-    <div class="filter-aside-search">
-      <h4>Поиск по названию:</h4>
-      <div class="search_wrapper">
-        <div class="search_container">
-          <input id="search" type="text">
+      <div class="filter-aside-search">
+        <h4>Поиск по названию</h4>
+        <div class="search_wrapper">
+          <div class="search_container">
+            <input id="search" type="text">
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="filter-aside-rating">
-      <h4>Рейтинг:</h4>
-    </div>
+      <div class="filter-aside-rating">
+        <h4>Рейтинг</h4>
+        <div class="rating_house">
+          <? foreach ($rating as $rat): ?>
+            <label for="rat<?= $rat ?>">
+              <input id="rat<?= $rat ?>" type="checkbox" name="rat<?= $rat ?>">
+              <span class="rat<?= $rat ?>"></span>
+            </label>
+          <? endforeach; ?>
+          <label class="rat0">
+            <input id="rat0" type="checkbox" name="rat0">
+            без рейтинга
+          </label>
+        </div>
+      </div>
 
+      <div class="filter-aside-cuisine">
+        <h4>Выбор кухни</h4>
+        <div class="cuisine">
+          <? foreach ($cuisine as $v => $c): ?>
+            <label for="cuisine<?= $v ?>">
+              <input id="cuisine<?= $v ?>" type="checkbox" name="cuisine<?= $v ?>">
+              <span><?= $c ?></span>
+            </label>
+          <? endforeach; ?>
+
+          <div id="cuisine_more">Другие...</div>
+          <div id="cuisine_more_list">
+            <? foreach ($cuisine_more as $v => $c): ?>
+              <label for="cuisine_more<?= $v ?>">
+                <input id="cuisine_more<?= $v ?>" type="checkbox" name="cuisine_more<?= $v ?>">
+                <span><?= $c ?></span>
+              </label>
+            <? endforeach; ?>
+          </div>
+        </div>
+      </div>
+
+      <div class="filter-aside-services">
+        <h4>Услуги и удобства</h4>
+        <div class="services">
+          <? foreach ($services as $v => $s): ?>
+            <label for="service<?= $v ?>">
+              <input id="service<?= $v ?>" type="checkbox" name="service<?= $v ?>">
+              <span class="service<?= $v ?>"></span>
+              <?= $s ?>
+            </label>
+          <? endforeach; ?>
+        </div>
+      </div>
+
+    </div>
   </div>
-</div>
+</form>
 
 <script>
   $(document).ready(function() {
+    // Choose price
     price_slider(<?= $min_price ?>, <?= $max_price ?>);
+    // Search by name
     $("#search").autocomplete({
-      source: ["Bar", "Bann", "Bre", "Bon"]
+      source: <?= json_encode($cuisine) ?>
     });
+
+    // show/hide more cuisines
+    $("#cuisine_more").click(function() {
+      if ($("#cuisine_more_list").is(":hidden")) {
+        $("#cuisine_more_list").show("slow");
+      } else {
+        $("#cuisine_more_list").hide("slow");
+      }
+      return false;
+    });
+
+
   });
 </script>
